75bd8713629713358ec028bbcebc8b3abc92b559
Tracy Speed <tspeed@me.com>
2018-06-26 19:12:16 -0500

    end of day work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+
+  namespace :api do
+    get "/one_recipe_url" => "recipes#one_recipe_action"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+
+  contact1 = Contact.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.cell_phone)
+
+  contact1.save
+
+end
